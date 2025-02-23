7ca527625e01a2afc17b8db223fcfb82ff6a4431
Alexone74 <alexone74@gmail.com>
2025-02-23 23:22:15 +0100

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-// app.js
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { QuizApp } from './QuizApp.js';
-
-const container = document.getElementById('root');
-const root = createRoot(container);
-root.render(React.createElement(QuizApp));
